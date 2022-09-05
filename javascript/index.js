4a6596c2d09d08ff73d5e48d6905a6f6e226b533
RuthvikSai2015 <84248537+RuthvikSai2015@users.noreply.github.com>
2022-09-05 21:14:17 +0530

    Javascript

---
+++ javascript/index.js
@@ -0,0 +1,6 @@
+        var a=10+20;
+        console.log("a--------"+a);
+        document.write("Welcome to the javascript session");
+        alert("Welcome to the javascript session");
+        confirm("Welcome to the javascript session");     
+        console.log("Welcome to the javascript session")
