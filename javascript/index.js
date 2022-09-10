4c4c4dfc18514bd86506add0abaa053843f42f2a
RuthvikSai2015 <84248537+RuthvikSai2015@users.noreply.github.com>
2022-09-10 09:09:19 +0530

    Javascript session

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,38 @@
-        var a=10+20;
-        console.log("a--------"+a);
-        document.write("Welcome to the javascript session");
-        alert("Welcome to the javascript session");
-        confirm("Welcome to the javascript session");     
-        console.log("Welcome to the javascript session")
+        // var a=10+20;
+        // console.log("a--------"+a);
+        // document.write("Welcome to the javascript session");
+        // alert("Welcome to the javascript session");
+        // confirm("Welcome to the javascript session");     
+        // console.log("Welcome to the javascript session")
+
+        // var x=10;
+        // console.log("Number 1-----",typeof(x));
+        // x=9.6;
+        // console.log("Number 2-----",typeof(x));
+        // x=false
+        // console.log("data type1-----",typeof(x));
+        // x="krishna"
+        // console.log("data type2-----",typeof(x));
+
+        // var n1,n2,t;
+
+        // n1 = 100;
+        // n2 = 200;
+        // console.log(n1,n2);
+        // t = n1; // t=100
+        // n1 = n2 // n1=200
+        // n2 = t // n2 = 100
+        // console.log(n1,n2);
+
+        var x= 200;
+        console.log(x+=100); // x=x+100 300
+
+        console.log(x-=50); //x=x-50 250
+        console.log(x*=4); // x=x*4 1000
+        console.log(x/=100); // x=x/100 10
+        console.log(x/=30); // 0.33
+        console.log(x%=10); // 0.33
+
+        
+
+
