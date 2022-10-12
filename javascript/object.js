44b772463dea51181fcd6bb93347646bbfdc8a9e
RuthvikSai2015 <84248537+RuthvikSai2015@users.noreply.github.com>
2022-10-12 12:56:04 +0530

    Interview Questions

---
+++ javascript/object.js
@@ -0,0 +1,20 @@
+let student = {
+    "rollNumber": 501,
+    "age": 23,
+    "height": 6.2,
+    "weight": 54,
+    "percentage": 78.63,
+    "grade": 'A',
+    "firstName": "Krish",
+    "lastName": "Kumar",
+    "hobbies": ['Singing', 'dancing', 'cooking'],
+    "phoneNumbers": [9876543212, 123456789],
+    "languages": ['English', 'Hindi'],
+    "address": {
+        "hNo": "5-8/36",
+        "roadNumber": 8,
+        "street": "K R Puram",
+        "pincode": 560043
+    }
+}
+console.log(student);
