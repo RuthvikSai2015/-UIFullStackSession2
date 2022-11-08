1f016d6d9a5492d48f01c70cefa9d5eaf832490f
RuthvikSai2015 <84248537+RuthvikSai2015@users.noreply.github.com>
2022-11-08 20:30:47 +0530

    added classes

--- typescript/employee.ts
+++ typescript/employee.ts
@@ -1,26 +1,32 @@
-class Employee {
+class LocationEmp{
+    constructor(public latitude:number,public longitude:number){
 
-    public empname: string;
-    public empage: number;
-    public maritalStatus: boolean;
-    public height: number;
-    
-    constructor(name:string,age:number,maritalStatus:boolean,height:number){
-
-        this.empname = name;
-        this.empage = age;
-        this.maritalStatus = maritalStatus;
-        this.height = height;
     }
+}
+class Address{
+    constructor(public houseNumber:string,public city:string,public pincode:number,public location:LocationEmp){
+    }
+}
+
+class Employee {
+    //DATA
+    
+    constructor(public empname:string,public empage:number,public maritalStatus:boolean,
+        public height:number,public hobbies:string[],public address:Address){
+    }
+    //Methods
     public showEmployeeName() {
         console.log(`Employee Name is ${this.empname}`);
     }
     public showEmployeeAge() {
         console.log(`Employee age is ${this.empage}`);
     }
-    public hobbies(hobbies: string){
+    public hobbiesEmployees(){
         //Ram's hobbies are singing,playing 
-        console.log(`${this.empname}'s hobbies are ${hobbies}`);
+        console.log(`${this.empname}'s hobbies are }`);
+        this.hobbies.forEach((hobby) => {
+            console.log(hobby);
+        })
     }
     public showAllInfo(){
         //EmpName :   EmpAge  MaritalStatus: height
@@ -28,10 +34,21 @@ class Employee {
         console.log(`Employee Age:   ${this.empage} \n`);
         console.log(`Marital Status:   ${this.maritalStatus} \n`);
         console.log(`Employee Height:   ${this.height} \n`);
+        console.log(`House Number----------${this.address.houseNumber}`);
+        console.log(`City----------${this.address.city}`);
+        console.log(`Pin Code----------${this.address.pincode}`);
+        console.log(`${this.empname}'s hobbies are }`);
+        this.hobbies.forEach((hobby) => {
+            console.log(hobby);
+        })
+        console.log(`location of employee--${this.address.location.latitude}`);
+        console.log(`location of employee--${this.address.location.longitude}`);
     }
 }
-let emp = new Employee('Savitha',28,true,4.7);
+let emplocation = new LocationEmp(12.4,54.5);
+let address = new Address('5-1',"Bangalore",560036,emplocation)
+let emp = new Employee('Savitha',28,true,4.7,['singing','playing','dancing'],address);
 // emp.showEmployeeName();
 // emp.showEmployeeAge();
-emp.hobbies("Singing,Playing,Dancing");
+//emp.hobbiesEmployees();
 emp.showAllInfo();
