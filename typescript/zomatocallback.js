a2a39ba518970bab174c98654947147c42c2e1a2
RuthvikSai2015 <84248537+RuthvikSai2015@users.noreply.github.com>
2022-11-28 20:57:59 +0530

    back-end

--- typescript/zomatocallback.js
+++ typescript/zomatocallback.js
@@ -2,13 +2,34 @@ let order = (call_production) => {
     setTimeout(() => {
         console.log("Order is placed");
         call_production();
-    },2000);
-   
+    }, 2000);
+
 }
 let production = () => {
-    setTimeout(()=>{
-        console.log("Order is confirmed by restaurant")
-    },500);
+    setTimeout(() => {
+        console.log("Order is confirmed by restaurant");
+        setTimeout(() => {
+            console.log("Food is Preparing");
+            setTimeout(() => {
+                console.log("Order is ready");
+                setTimeout(() => {
+                    console.log("Order has been packed");
+                    setTimeout(() => {
+                        console.log("Delivery boy has picked up your order");
+                        setTimeout(() => {
+                            console.log("Delivery boy on the way");
+                            setTimeout(() => {
+                                console.log("Delivery boy has reached your home")
+                                setTimeout(() => {
+                                    console.log("Order is delivered")
+                                }, 5000);
+                            }, 3000);
+                        }, 5000);
+                    }, 2000);
+                }, 3 * 1000)
+            }, 1 * 1000)
+        }, 10 * 1000)
+    }, 500);
 }
 //callback
 order(production);
