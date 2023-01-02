af14144e361a69474b6059303ce1b8c9e1bd5c96
RuthvikSai2015 <84248537+RuthvikSai2015@users.noreply.github.com>
2023-01-02 20:33:47 +0530

    react

--- javascript/callbacknode.js
+++ javascript/callbacknode.js
@@ -57,22 +57,84 @@ var events = require("events");
 
 // console.log("Program Ended");
 
-var fs = require('fs');
-var fileData = "Sample Write Stream";
-//create a readable stream
-var writable = fs.createWriteStream('output.txt');
+// var fs = require('fs');
+// var fileData = "Sample Write Stream";
+// //create a readable stream
+// var writable = fs.createWriteStream('output.txt');
 
-// Set the encoding to be utf8
-writable.write(fileData,"UTF8");
+// // Set the encoding to be utf8
+// writable.write(fileData,"UTF8");
 
-//Handle Stream Event finish and error
-writable.on('finish',function(){
-  console.log("Write Completed");
+// //Handle Stream Event finish and error
+// writable.on('finish',function(){
+//   console.log("Write Completed");
+// })
+
+
+// writable.on('error',function(err){
+//     console.log(err.stack);
+// });
+
+// console.log("Program Ended");
+
+//Piping 
+
+// var fs = require('fs');
+
+// //create a readable stream
+// var readerStream = fs.createReadStream('input.txt');
+
+// // create a writable stream 
+// var writerStream = fs.createWriteStream('output1.txt')
+
+// // Pipe the read and write operations
+// // read input.txt and write data to output.txt
+// readerStream.pipe(writerStream);
+
+// console.log("Program Ended");
+// //Chaining of Streams
+// fs.createReadStream("input.txt").pipe(zlib.createZIP()).pipe(fs.createWriteStream());
+
+//File System 
+
+// var fs = require('fs');
+
+//Asynchronous read
+// fs.readFile('input.txt', function(err,data){
+//     if(err){
+//       return console.log(err);
+//     }
+//     console.log("data from file ----"+data.toString());
+// })
+// //Synchronous read
+// var data = fs.readFileSync('input.txt');
+// console.log("data---- "+data.toString());
+
+// console.log("Program Ended")
+//Global Objects
+// console.log("directory name --",__dirname);
+// console.log("File Name---",__filename);
+// setTimeout(callback,time)
+// clearTimeout()
+// setInterval(callback,time)
+// clearInterval()
+
+// console and process
+
+// console.log("Log Message");  //print the data to standard output
+// console.error("Error Message"); // print the data to standard error
+// console.info("Information Message");
+// console.warn("Warning Message");
+// console.trace();
+
+//process object 
+process.on('exit',function(code){
+  setTimeout(() => {
+      console.log("code run")
+  }, 500);
+
+  console.log("Exit code",code);
 })
 
-
-writable.on('error',function(err){
-    console.log(err.stack);
-});
-
+console.log(process.env);
 console.log("Program Ended");
